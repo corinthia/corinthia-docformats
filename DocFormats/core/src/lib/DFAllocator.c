701c6c4e5c522936840cfe0bdf80838c3a6d704d
Peter Kelly <peter@uxproductivity.com>
2015-02-27 02:32:43 +0700

    Add a couple of missing #includes for xmalloc

--- DocFormats/core/src/lib/DFAllocator.c
+++ DocFormats/core/src/lib/DFAllocator.c
@@ -15,6 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "DFPlatform.h"
 #include "DFAllocator.h"
 #include "DFCommon.h"
 #include <assert.h>
