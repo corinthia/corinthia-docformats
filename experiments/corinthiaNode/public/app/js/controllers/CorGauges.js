92d838d6947a8b28be9a971ce34e04253bc110f6
ianc <ianc@amham.net>
2016-05-29 16:59:25 +0800

    Highlight gauge changes.

--- experiments/corinthiaNode/public/app/js/controllers/CorGauges.js
+++ experiments/corinthiaNode/public/app/js/controllers/CorGauges.js
@@ -15,6 +15,8 @@
 
         vm.gaugeFilter = gaugeFilter;
         vm.noGauges = noGauges;
+        vm.gaugeStr = gaugeStr;
+        vm.gaugeClass = gaugeClass;
 
         init();
 
@@ -70,29 +72,51 @@
             for (var ns = 0; ns < coverage.length; ns++) {
                 var nsgauges = coverage[ns].gauges;
                 for (var g = 0; g < nsgauges.length; g++) {
+                    vm.coverage[ns].gauges[g].orig = -1;
                     if (adding) {
                         if (nsgauges[g].value) {
+                            vm.coverage[ns].gauges[g].orig = vm.coverage[ns].gauges[g].value;
                             vm.coverage[ns].gauges[g].value += nsgauges[g].value;
                             vm.coverage[ns].total += nsgauges[g].value;
                         }
                     } else {
                         if (nsgauges[g].value) {
+                            vm.coverage[ns].gauges[g].orig = vm.coverage[ns].gauges[g].value;
                             vm.coverage[ns].gauges[g].value -= nsgauges[g].value;
                             vm.coverage[ns].total -= nsgauges[g].value;
                         }
                     }
                 }
             }
-
         }
 
         //don't like the negative logic but feeding a disabled
-	function noGauges(test) {
+        function noGauges(test) {
             var retval = true;
-            if(test.gauges && test.gauges != "") {
+            if (test.gauges && test.gauges !== "") {
                 retval = false;
             }
-            return retval; 
+            return retval;
+        }
+
+        function gaugeStr(gauge) {
+            if (gauge.orig && gauge.orig >=0 ) {
+                return gauge.orig + " -> " + gauge.value;
+            } else {
+                return gauge.value;
+            }
+        }
+
+        function gaugeClass(gauge) {
+            if(gauge.value) {
+                if(gauge.orig >= 0) {
+                    return "success";
+                } else {
+                    return "info";
+                }
+            } else {
+                return "info";
+            }
         }
     }
 })();
